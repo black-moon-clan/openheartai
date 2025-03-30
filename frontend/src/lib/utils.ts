d9ad5e6b274557db8e9d58a5785416669bc40102
ozzyworld <ozzyworld@example.com>
2025-03-30 14:43:44 -0700

    Remove unused MetaMask code to fix runtime error

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,30 +1,6 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-declare global {
-  interface Window {
-    ethereum?: {
-      isMetaMask?: boolean;
-      request: (args: {
-        method: string;
-        params?: unknown[];
-      }) => Promise<unknown>;
-    };
-  }
-}
-
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-export const getEthereumProvider = () => {
-  if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
-    return window.ethereum;
-  }
-  return null;
-};
-
-export const isMetaMaskInstalled = () => {
-  const ethereum = getEthereumProvider();
-  return ethereum && ethereum.isMetaMask;
-};
